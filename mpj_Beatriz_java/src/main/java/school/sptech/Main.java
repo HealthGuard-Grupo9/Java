c28f03c20ac0cd6837b2a00945ebd36fab29e644
beatrizcarvalho005 <beatriz.carvalho@sptech.com>
2025-09-06 11:04:21 -0300

    agregando o meu projeto em java

---
+++ mpj_Beatriz_java/src/main/java/school/sptech/Main.java
@@ -0,0 +1,105 @@
+package school.sptech;
+
+import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        List<String> tarefas = new ArrayList<>();
+
+        Scanner leitor = new Scanner(System.in);
+
+
+        while (true) {
+            System.out.println("------------------------------------------");
+            System.out.println(" HealthTask — simplificando sua rotina. ");
+            System.out.println("------------------------------------------");
+            System.out.println("1 - Ver tarefas");
+            System.out.println("2 - Cadastrar tarefa");
+            System.out.println("3 - Excluir tarefa");
+            System.out.println("0 - Sair");
+            System.out.println("------------------------------------------");
+            System.out.print("Digite: ");
+
+
+
+
+            int numero = leitor.nextInt();
+            leitor.nextLine();
+
+
+
+            if (numero == 1) {
+                if (tarefas.isEmpty()) {
+                    System.out.println("Nenhuma tarefa cadastrada ainda!");
+                }
+
+                else {
+                    System.out.println("Tarefas cadastradas:");
+
+                    for (int i = 0; i < tarefas.size(); i++) {
+
+                        System.out.println((i + 1) + " - " + tarefas.get(i));
+
+                    }
+                }
+
+
+            }
+            else if (numero == 2) {
+
+                System.out.print("Digite a tarefa: ");
+
+                String tarefa = leitor.nextLine();
+
+                tarefas.add(tarefa);
+
+                System.out.println("Tarefa cadastrada!");
+            }
+
+            else if (numero == 3) {
+
+                if (tarefas.isEmpty()) {
+                    System.out.println("Não há tarefas para excluir!");
+                }
+
+                else {
+                    System.out.println("Escolha a tarefa para excluir:");
+
+                    for (int i = 0; i < tarefas.size(); i++) {
+
+                        System.out.println((i + 1) + " - " + tarefas.get(i));
+                    }
+
+                    System.out.print("Digite o número da tarefa: ");
+
+                    int numeroTarefa = leitor.nextInt();
+
+                    leitor.nextLine();
+
+                    if (numeroTarefa >= 1 && numeroTarefa <= tarefas.size()) {
+
+                        String removida = tarefas.remove(numeroTarefa - 1);
+                        System.out.println("Tarefa \"" + removida + "\" excluída!");
+                    }
+
+                    else {
+                        System.out.println("Número inválido!");
+                    }
+                }
+            } else if (numero == 0) {
+                System.out.println("Encerrando...");
+                break;
+            } else {
+                System.out.println("Por favor, escolha uma opção válida!");
+            }
+        }
+
+
+        leitor.close();
+    }
+
+}
